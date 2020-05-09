55eff74ef7b65d84ea2682a1271a911da4e23f8c
Tom Cole <tomcole@mac.com>
2020-05-09 12:45:55 -0400

    Add Format() function for abstract values

---
+++ expressions/format.go
@@ -0,0 +1,36 @@
+package expressions
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Format converts the given object into a string representation.
+// In particular, this varies from a simple "%v" format in Go because
+// it puts commas in the array list output to match the syntax of an
+// array constant and puts quotes around string values.
+func Format(arg interface{}) string {
+
+	switch v := arg.(type) {
+
+	case []interface{}:
+
+		var b strings.Builder
+		b.WriteRune('[')
+
+		for n, i := range v {
+			if n > 0 {
+				b.WriteString(", ")
+			}
+			b.WriteString(Format(i))
+		}
+		b.WriteRune(']')
+		return b.String()
+
+	case string:
+		return "\"" + v + "\""
+
+	default:
+		return fmt.Sprintf("%v", v)
+	}
+}
---
+++ expressions/format_test.go
@@ -0,0 +1,40 @@
+package expressions
+
+import "testing"
+
+func TestFormat(t *testing.T) {
+	tests := []struct {
+		name string
+		arg  interface{}
+		want string
+	}{
+		{
+			name: "Integer",
+			arg:  33,
+			want: "33",
+		},
+		{
+			name: "Float",
+			arg:  10.5,
+			want: "10.5",
+		},
+		{
+			name: "Array of int",
+			arg:  []interface{}{3, 5, 55},
+			want: "[3, 5, 55]",
+		},
+		{
+			name: "Array with array",
+			arg:  []interface{}{3, []interface{}{"tom", true}, 55},
+			want: "[3, [\"tom\", true], 55]",
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Format(tt.arg); got != tt.want {
+				t.Errorf("Format() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
