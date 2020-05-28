32b963c2796e56d00a76dc3e1713f2e604ffb581
Tom Cole <tomcole@mac.com>
2020-05-28 18:42:31 -0400

    Temp symbol names
    Make temporary symbol names generated by the compiler (typically
    if a for-loop or a switch statement) less likely to collide
    with end-user names.

--- compiler/utility.go
+++ compiler/utility.go
@@ -1,23 +1,14 @@
 package compiler
 
 import (
-	"strconv"
-	"sync"
+	"github.com/google/uuid"
 )
-
-var index = 0
-var indexMux sync.Mutex
 
 // MakeSymbol creates a unique symbol name for use
 // as a temporary variable, etc. during compilation.
 func MakeSymbol() string {
 
-	var i int
+	x := uuid.New().String()
 
-	indexMux.Lock()
-	index = index + 1
-	i = index
-	indexMux.Unlock()
-
-	return "COMPILER_TEMP__" + strconv.Itoa(i)
+	return "#" + x[28:]
 }
