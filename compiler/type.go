a2b16ff12a9f4b7fb574f89c005cd57d1e051f7f
Tom Cole <tomcole@mac.com>
2020-06-06 15:08:01 -0400

    Revised type statement syntax

--- compiler/type.go
+++ compiler/type.go
@@ -2,7 +2,6 @@ package compiler
 
 import (
 	"github.com/tucats/gopackages/bytecode"
-	"github.com/tucats/gopackages/expressions"
 	"github.com/tucats/gopackages/tokenizer"
 )
 
@@ -17,15 +16,13 @@ func (c *Compiler) Type() error {
 	if c.t.Peek(1) != "{" {
 		return c.NewTokenError("expected {, found ")
 	}
+	c.b.Emit2(bytecode.Push, name)
 
 	// Compile a struct definition
-	ex, err := expressions.Compile(c.t)
+	err := c.compileType()
 	if err != nil {
 		return err
 	}
-
-	c.b.Emit2(bytecode.Push, name)
-	c.b.Append(ex)
 
 	// Add in the type linkage, and store as the type name. The __type for a type is
 	// a string that is the name of the type. When a member dereference on a struct
@@ -36,5 +33,52 @@ func (c *Compiler) Type() error {
 	c.b.Emit2(bytecode.SymbolCreate, name)
 	c.b.Emit2(bytecode.Store, name)
 
+	return nil
+}
+
+func (c *Compiler) compileType() error {
+
+	// Must start with {
+	if !c.t.IsNext("{") {
+		return c.NewError("expected { not found")
+	}
+
+	count := 0
+	for true {
+		name := c.t.Next()
+		if !tokenizer.IsSymbol(name) {
+			return c.NewStringError("invalid member name", name)
+		}
+		count = count + 1
+		if c.t.Peek(1) == "{" {
+			err := c.compileType()
+			if err != nil {
+				return err
+			}
+		} else {
+			switch c.t.Next() {
+			case "int":
+				c.b.Emit2(bytecode.Push, 0)
+			case "float":
+				c.b.Emit2(bytecode.Push, 0.0)
+			case "bool":
+				c.b.Emit2(bytecode.Push, false)
+			case "string":
+				c.b.Emit2(bytecode.Push, "")
+			default:
+				return c.NewTokenError("invalid type")
+			}
+		}
+
+		c.b.Emit2(bytecode.Push, name)
+
+		if c.t.IsNext("}") {
+			c.b.Emit2(bytecode.Struct, count)
+			return nil
+		}
+		if c.t.AtEnd() {
+			return c.NewError("incomplete type definition")
+		}
+	}
 	return nil
 }
