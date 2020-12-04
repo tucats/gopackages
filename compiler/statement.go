a3255116759cd565d9aa3d02a05bbfb946acb6c0
Tom Cole <tomcole@mac.com>
2020-12-03 19:40:13 -0500

    bug: fix unrecognized statement error msg token

--- compiler/statement.go
+++ compiler/statement.go
@@ -90,5 +90,5 @@ func (c *Compiler) Statement() error {
 	}
 
 	// Unknown statement, return an error
-	return c.NewError(UnexpectedTokenError, c.t.Peek(1))
+	return c.NewError(UnexpectedTokenError, c.t.Peek(0))
 }
